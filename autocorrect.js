14edae9db166f4cf908849aab13565396ca99b98
contactkalapaniyatra-create <contact.kalapaniyatra@gmail.com>
2026-02-08 12:36:55 +0530

    Add files via upload

---
+++ autocorrect.js
@@ -0,0 +1,23 @@
+// Wrong -> Correct words
+const autocorrect = [
+  {
+    correct: "javascript",
+    wrong: ["javscrit", "javascrit", "javscript", "java script", "javasript", "js"]
+  },
+  {
+    correct: "html",
+    wrong: ["htlm", "hmtl", "hypertext", "hyper text"]
+  },
+  {
+    correct: "css",
+    wrong: ["cs", "ccs", "style", "styling"]
+  },
+  {
+    correct: "hello",
+    wrong: ["helo", "hi", "hey", "helloo"]
+  },
+  {
+    correct: "error",
+    wrong: ["eror", "err", "bug", "issue", "mistke"]
+  }
+];
